6a6a6ee7bc091974fd2cac2ea26dddf1344b049a
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-01-16 22:03:43 -0500

    Initial commit after the initial working model

---
+++ carsapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Car,Comment
+
+# Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Comment)
---
+++ carsapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+# tunr/models.py
+class Car(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    make = models.CharField(max_length=100, null=True)
+    model = models.CharField(max_length=100, null=True)
+    year = models.CharField(max_length=4, null=True)
+    color = models.CharField(max_length=100, null=True)
+    trim = models.CharField(max_length=100, null=True)
+    photo_url = models.CharField(max_length=255, null=True)
+    description = models.TextField(max_length=1000, null=True)
+
+    def __str__(self):  
+        return self.name
+    
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=500)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='cars')
+
+    def __str__(self):
+        return self.comment
