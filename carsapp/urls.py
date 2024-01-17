afb030ef26e683ce7aa9d4c0edc93df481df6cca
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-01-16 22:34:07 -0500

    ga-adding templates and views and css

---
+++ carsapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.car_list, name='car_list'),
+  path('cars/<int:pk>', views.car_detail, name='car_detail')
+]
--- carsapp/views.py
+++ carsapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Car, Comment
+
+def car_list(request):
+  cars = Car.objects.all()
+  return render(request, 'carsapp/car_list.html', {'cars',cars})
+
+def car_detail(request,pk):
+  car = Car.objects.get(id=pk)
+  return render(request, 'carsapp/car_detail.html')
+
+def comment_list(request):
+  comments = Comment.objects.all()
+  return render(request, 'carsapp/comments_list.html',{'comments',comments})
