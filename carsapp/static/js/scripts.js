dbf08dd1979fb6eb32c9c939db6151412beddd08
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-01-24 22:35:48 -0500

    ga-added tooltip on collection icon

--- carsapp/static/js/scripts.js
+++ carsapp/static/js/scripts.js
@@ -2,3 +2,21 @@
 function scrollTo(hash) {
   location.hash = '#' + hash;
 }
+console.log('scripts.js loaded');
+const tooltips = document.querySelectorAll('.tooltip');
+const favorites = document.querySelectorAll('.favorite');
+
+favorites.forEach(function (favorite) {
+  favorite.addEventListener('mouseover', function () {
+    console.log('favorite');
+    tooltips.forEach(function (tooltip) {
+      tooltip.classList.add('show');
+    });
+  });
+
+  favorite.addEventListener('mouseout', function () {
+    tooltips.forEach(function (tooltip) {
+      tooltip.classList.remove('show');
+    });
+  });
+});
