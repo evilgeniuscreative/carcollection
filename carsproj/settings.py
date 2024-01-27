80e946d3e50d4437cbce18173620d8b9ebfa6c5f
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-01-27 12:13:42 -0500

    Set STATIC_ROOT in settings.py

--- carsproj/settings.py
+++ carsproj/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
